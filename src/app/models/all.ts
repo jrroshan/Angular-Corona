3831e562c8f2ca817798c247dac1e61ada931b95
Roshan Lasiwa <roshanlasiwa@gmail.com>
2020-06-12 19:26:12 +0545

    added all and countries model

---
+++ src/app/models/all.ts
@@ -0,0 +1,24 @@
+export class all{
+    cases:number;
+    deaths:number;
+    recovered:number;
+    todayCases:number;
+}
+
+export class countries{
+    country:string;
+    countryInfo:{
+        flag:string;
+    };
+    cases:number;
+    todayCases:number;
+    deaths:number;
+    todayDeaths:number;
+    recovered:number;
+    active:number;
+    critical:number;
+    todayRecovered:number;
+    tests:number;
+    population:number;
+    testsPerOneMillion:number;
+}
