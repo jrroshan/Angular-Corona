45a1f98cf4c7f2ce83899633fc1d84652524466c
Roshan Lasiwa <roshanlasiwa@gmail.com>
2020-06-12 19:24:02 +0545

    routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContentComponent } from "../app/content/content/content.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path:"",
+  component: ContentComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
